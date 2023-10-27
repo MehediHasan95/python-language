66ba718bdc17976a5a5a8f4a654f977d5041707d
MehediHasan95 <mehedihasan5131@gmail.com>
2023-10-27 22:18:50 +0600

    string added

--- string.py
+++ string.py
@@ -1,3 +1,25 @@
-university = 'Stamford University Bangladesh'
-print(len(university)) # length: 30
+'''In computer programming, a string is a sequence of characters.'''
 
+university = 'Stamford University Bangladesh'
+print(len(university))  # length: 30
+
+print(university[6])  # r
+
+print(university[-4])  # d
+
+print("u" in university)  # False
+print("v" in university)  # True
+print("U" not in university)  # False
+
+print(university[10:20])  # niversity
+
+# multiline string
+message = """
+Never gonna give you up
+Never gonna let you down
+"""
+print(message)
+
+greet = 'Hello'
+for word in greet:
+    print(word)
